405b9be432458d5a1711541b1b12b1e77b03071c
Obum <obumnwabude@gmail.com>
2025-02-27 17:41:21 +0100

    feat: assign instance role for vendor on newly created order

--- server/routes/orders/index.post.ts
+++ server/routes/orders/index.post.ts
@@ -2,14 +2,21 @@ export default defineEventHandler(async (event) => {
   const { newOrder, orders } = event.context;
   orders.unshift(newOrder);
   saveOrders(orders);
-  const { id } = newOrder;
+  const { id, totalPrice, vendor } = newOrder;
 
   // Sync the new order with Permit
   await permit.api.resourceInstances.create({
     key: id,
     resource: 'Order',
-    attributes: { cost: newOrder.totalPrice },
+    attributes: { cost: totalPrice },
     tenant: 'default'
+  });
+
+  // Set the Order's Vendor with Permit
+  await permit.api.roleAssignments.assign({
+    user: vendor,
+    role: 'Vendor',
+    resource_instance: `Order:${id}`
   });
 
   return { id };
