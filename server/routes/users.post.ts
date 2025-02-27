d91eeb337fb2d1fb213d00fa8b92615b5aba6043
Obum <obumnwabude@gmail.com>
2025-02-27 16:58:52 +0100

    chore: use users.sync for updating users

--- server/routes/users.post.ts
+++ server/routes/users.post.ts
@@ -2,17 +2,13 @@ export default defineEventHandler(async (event) => {
   const { user, role, isGrant } = await readBody(event);
 
   // Create User with Permit not existing
-  try {
-    await permit.api.users.get(user);
-  } catch (error) {
-    await permit.api.users.create({ key: user });
-  }
+  await permit.api.users.sync({ key: user });
 
   // Assign or Unassign Role in default tenant depending on isGrant
   const tenant = 'default';
   if (isGrant) {
     await permit.api.roleAssignments.assign({ user, role, tenant });
   } else {
-    await permit.api.roleAssignments.unassign({ user, role,  tenant });
+    await permit.api.roleAssignments.unassign({ user, role, tenant });
   }
 });
