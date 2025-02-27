6fdaef810be998270ec8ab76012270f10c3e8938
Obum <obumnwabude@gmail.com>
2025-02-27 16:56:58 +0100

    chore: use users.sync for updating users and remove code-based tenant update

--- server/routes/users.post.ts
+++ server/routes/users.post.ts
@@ -1,19 +1,8 @@
 export default defineEventHandler(async (event) => {
   const { city: tenant, user, role, isGrant } = await readBody(event);
 
-  // Create Tenant if it doesn't exist
-  try {
-    await permit.api.tenants.get(tenant);
-  } catch (error) {
-    await permit.api.tenants.create({ key: tenant, name: tenant });
-  }
-
-  // Create User with Permit not existing
-  try {
-    await permit.api.users.get(user);
-  } catch (error) {
-    await permit.api.users.create({ key: user });
-  }
+  // Create User with Permit if not existing
+  await permit.api.users.sync({ key: user });
 
   // Assign or Unassign Role depending on isGrant
   if (isGrant) {
