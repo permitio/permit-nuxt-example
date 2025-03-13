c15b9ffeb8ed2a50222e69f1c78b7b1a88e140d0
Obum <obumnwabude@gmail.com>
2025-03-13 14:55:42 +0100

    fix: remove hardcoded local PDP link

--- server/utils/permit.ts
+++ server/utils/permit.ts
@@ -2,6 +2,6 @@ import { Permit } from 'permitio';
 
 const config = useRuntimeConfig(); // using Nuxt runtime to get env vars
 export const permit = new Permit({
-  pdp: 'http://localhost:7766', // config.permitPdp,
+  pdp: config.permitPdp,
   token: config.permitToken
 });
